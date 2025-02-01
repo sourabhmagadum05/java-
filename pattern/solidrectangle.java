579ad574f7965451b17cac73bfa9f37aadaf7442
sourabhmagadum05 <sourabhmagdum27@gamil.com>
2025-02-01 12:48:02 +0530

    this is solidrectangle pattern cod

---
+++ pattern/solidrectangle.java
@@ -0,0 +1,16 @@
+package pattern;
+
+import loops.student;
+
+public class solidrectangle {
+
+    public static void main(String arag[]){
+
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 5; j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
